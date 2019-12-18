a571c79e6a73d26c3804acb965027093d39c75b4
Yashkhatsuriya <49470851+Yashkhatsuriya@users.noreply.github.com>
2019-12-17 18:37:09 -1000

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPrac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllArea all = new AllArea();
+            Console.Write("Enter Height of square:");
+            int x=Convert.ToInt32(Console.ReadLine());
+            all.area(x);
+
+
+            Console.Write("Enter Height of Reactangle:");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Length of Reactangle:");
+            int l = Convert.ToInt32(Console.ReadLine());
+            all.area(h, l);
+
+
+            Console.Write("Enter Redius of Circle:");
+            int r = Convert.ToInt32(Console.ReadLine());
+            all.area(r);
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class AllArea
+    {
+        public void area(int h)
+        {
+            int a = h * h;
+            Console.WriteLine("Area of square:"+a);
+           // Console.ReadLine();
+        }
+        public void area(int h, int l)
+        {
+            int a = l * h;
+            Console.WriteLine("area of ractangle:"+ a);
+            //Console.ReadLine();
+        }
+        public void area( int r, double pi = 3.14)
+        {
+            double a = pi * r * r;
+            Console.WriteLine("Area of circle:"+ a);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secondprac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[50];
+            int min, max, n, i;
+
+            Console.Write("Input number of element to input in array:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("input {0} elements in array\n", n);
+            for(i=0;i<n;i++)
+            {
+                Console.Write("Element -{0}:",i);
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+            min = arr[0];
+            max = arr[0];
+
+            for(i=0;i<n;i++)
+            {
+                if(arr[i]>max)
+                {
+                    max = arr[i];
+                }
+                if(arr[i]<min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.Write("Maximum elements value is{0}\n",max);
+            Console.Write("Minimum elements value is{0}\n", min);
+            Console.ReadLine();
+
+        }
+    }
+}
